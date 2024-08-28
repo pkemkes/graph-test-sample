b397cd981c1652a9fd8d84e4644ddf0f8ad9f9aa
Phillip Kemkes <Phillip.Kemkes@gdata.de>
2024-08-28 14:26:11 +0200

    use CreateProcess

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <windows.h>
 
 
 int write_eicar(char filename[]) {
@@ -21,11 +22,46 @@ int write_eicar(char filename[]) {
 }
 
 
+int run_command(char command[]) {
+    // https://learn.microsoft.com/en-us/windows/win32/procthread/creating-processes
+
+    STARTUPINFO si;
+    PROCESS_INFORMATION pi;
+
+    ZeroMemory( &si, sizeof(si) );
+    si.cb = sizeof(si);
+    ZeroMemory( &pi, sizeof(pi) );
+
+    // Start the child process. 
+    if(!CreateProcess(NULL,   // No module name (use command line)
+        command,        // Command line
+        NULL,           // Process handle not inheritable
+        NULL,           // Thread handle not inheritable
+        FALSE,          // Set handle inheritance to FALSE
+        0,              // No creation flags
+        NULL,           // Use parent's environment block
+        NULL,           // Use parent's starting directory 
+        &si,            // Pointer to STARTUPINFO structure
+        &pi)            // Pointer to PROCESS_INFORMATION structure
+    ) {
+        printf("CreateProcess failed (%d).\n", GetLastError());
+        return 1;
+    }
+
+    // Wait until child process exits.
+    WaitForSingleObject( pi.hProcess, INFINITE );
+
+    // Close process and thread handles. 
+    CloseHandle( pi.hProcess );
+    CloseHandle( pi.hThread );
+    return 0;
+}
+
+
 int main() {
     char filepath[] = "eicar.com";
     if (write_eicar(filepath) != 0) {
         return 1;
     }
-    system(filepath);
-    return 0;
+    return run_command(filepath);
 }
