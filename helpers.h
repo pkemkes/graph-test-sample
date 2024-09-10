e69ab6d2e07bc3026cc1299f250db48923cdf60c
Phillip Kemkes <Phillip.Kemkes@gdata.de>
2024-09-10 15:01:02 +0200

    replace beacon with C program

--- helpers.h
+++ helpers.h
@@ -2,3 +2,4 @@ void write_string(const char *filepath, const char *string);
 int run_command(char command[]);
 int run_powershell_script(char powerShellScriptPath[]);
 int write_to_startup_regkey(char name[], char command[]);
+int send_https_get_request(char url[]);
