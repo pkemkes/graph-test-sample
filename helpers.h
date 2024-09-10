c17db73888e7c1a8e27ea37b830ac06629b33283
Phillip Kemkes <Phillip.Kemkes@gdata.de>
2024-09-10 16:00:40 +0200

    move http request into own header

--- helpers.h
+++ helpers.h
@@ -2,4 +2,3 @@ void write_string(const char *filepath, const char *string);
 int run_command(char command[]);
 int run_powershell_script(char powerShellScriptPath[]);
 int write_to_startup_regkey(char name[], char command[]);
-int send_https_get_request(char url[]);
